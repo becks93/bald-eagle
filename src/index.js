f97cc0b5b9be5d498846d554ebef96b1fe27838d
Beck Spencer-Strong <beck.spencer-strong@1up.health>
2023-03-10 10:30:05 -0500

    adding everything

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Navigation,
+  Home,
+  Diet,
+  ImageGallery,
+  Anatomy,
+  Behavior,
+  Habitat,
+  ScientificName,
+  Lifecycle,
+} from "./components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/diet" element={<Diet />} />
+        <Route path="/imagegallery" element={<ImageGallery />} />
+        <Route path="/anatomy" element={<Anatomy />} />
+        <Route path="/behavior" element={<Behavior />} />
+        <Route path="/habitat" element={<Habitat />} />
+        <Route path="/lifecycle" element={<Lifecycle />} />
+        <Route path="/scientificname" element={<ScientificName />} />
+      </Routes>
+    </Router>
+    {/* <App /> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
